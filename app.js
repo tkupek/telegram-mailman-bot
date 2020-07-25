00ef0a38a4e11f046fff9bba0725911722031275
Timo Tomasini <timo@tomasini-mail.de>
2020-07-25 02:34:07 +0200

    Add draft for hopefully secure web setup routine

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const { body, query, validationResult } = require('express-validator');
+const bodyParser = require('body-parser');
+const pug = require('pug');
 const cron = require('node-cron');
-const { Telegraf } = require('telegraf')
+const { Telegraf } = require('telegraf');
 
 const data = require('./src/data');
 const handler = require('./src/handler');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Start Bot
 const bot = new Telegraf(process.env.BOT_TOKEN)
@@ -32,6 +36,34 @@ app.get('/_ah/stop', async (req, res) => {
 app.get('/_ah/start', async (req, res) => {
 	await bot.launch();
 	res.sendStatus(200);
+});
+
+app.get("/setup", [
+	query('id').isHash('sha1')
+], function (req, res) {
+	console.log("validating ...")
+	const validationError = validationResult(req);
+	if(!validationError.isEmpty()) {
+		return res.status(401).send('Missing or invalid session id ...');
+	}
+
+	const compiledSetup = pug.compileFile(__dirname + '/resources/setup.pug');
+	res.send(compiledSetup({sessionId: req.query.id}));
+});
+app.post('/setup', urlencodedParser, [
+	body('session').isHash('sha1'),
+	body('username').isString(),
+	body('password').isString(),
+	body('host').isURL({protocols: ['http', 'https']}),
+	body('xAuthHeader').isString()
+], function (req, res) {
+	const validationError = validationResult(req);
+	if(!validationError.isEmpty()) {
+		return res.status(400).json({ errors: validationError.array() });
+	}
+
+	res.send('welcome!\n');
+	console.log(req.body)
 });
 
 app.listen(process.env.PORT, () => {
